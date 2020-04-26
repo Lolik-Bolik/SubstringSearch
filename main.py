1a84bea65808c874d522ea7fe15eefd051d4e9a7
Pavel Semkin <pvsemk@mail.ru>
2020-04-26 16:37:58 +0300

    Completed karp_rabin

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import algorithms as algo
+import os
 
 
 def main():
-    with open('./benchmarks/bad_t_1.txt', 'r') as text:
-        text = text.read()
-    with open('./benchmarks/bad_w_1.txt', 'r') as pattern:
-        pattern = pattern.read()
-    #text = 'fdv'
-    #pattern = 'd'
-    b_m = algo.BruteForce(text, pattern)
-    result = b_m.search()
-    print(result)
+    reference_names = ['bad_t_1.txt', 'bad_t_2.txt', 'bad_t_3.txt', 'bad_t_4.txt',
+                       'good_t_1.txt', 'good_t_2.txt', 'good_t_3.txt', 'good_t_4.txt']
+    target_names = ['bad_w_1.txt', 'bad_w_2.txt', 'bad_w_3.txt', 'bad_w_4.txt',
+                    'good_w_1.txt', 'good_w_2.txt', 'good_w_3.txt', 'good_w_4.txt']
+    for reference, target in zip(reference_names, target_names):
+        with open(os.path.join('./benchmarks', reference)) as text_file:
+            text = text_file.read()
+        with open(os.path.join('./benchmarks', target)) as pattern_file:
+            pattern = pattern_file.read()
+        boyer = algo.KarpRabin(text, pattern)
+        results = boyer.search(True)
+        print(results)
+    #text = 'fdvddasdadvdgdvd'
+    #pattern = 'dvd'
 
 
 if __name__ == '__main__':
